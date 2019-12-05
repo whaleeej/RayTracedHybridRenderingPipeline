6d99dce85c1ce569a74d6148c88d9d815d929a5f
ceej <ceej_7@hotmail.com>
2019-12-05 10:52:01 +0800

    feat: repo init

---
+++ DirectX12-Demo/Helpers.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h> // For HRESULT
+
+// From DXSampleHelper.h 
+// Source: https://github.com/Microsoft/DirectX-Graphics-Samples
+inline void ThrowIfFailed(HRESULT hr)
+{
+	if (FAILED(hr))
+	{
+		throw std::exception();
+	}
+}
