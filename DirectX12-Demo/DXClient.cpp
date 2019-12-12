b805ebeef9ac326d0c0f7f7c4ac6bd6f5fb47d7d
ceej <ceej_7@hotmail.com>
2019-12-12 15:53:18 +0800

    fix: adapt to CommandQueueRequester

--- DirectX12-Demo/DXClient.cpp
+++ DirectX12-Demo/DXClient.cpp
@@ -1,16 +1,17 @@
 //#include <d3dcompiler.h>
 //#include <DirectXMath.h>
+#include <d3dx12.h>
 
 #include "DXClient.h"
-#include "DXFactory.h"
-#include "WinImpl.h"
+
 #include "Helpers.h"
+#include "Window.h"
+#include "DXFactory.h"
+#include "DXCommandRequester.h"
 
 DXClient::DXClient(bool useWarp, bool vSync, bool tearing):
 	g_UseWarp(useWarp), g_VSync(vSync), g_TearingSupported(tearing)
 {
-	g_FenceValue = 0;
-	
 	g_IsCounterInitialized = false;
 	frameCounter = 0;
 	elapsedSeconds = 0.0;
@@ -21,10 +22,10 @@ DXClient::DXClient(bool useWarp, bool vSync, bool tearing):
 
 DXClient::~DXClient()
 {
-	::CloseHandle(g_FenceEvent);
+	g_CommandRequester->flush();
 }
 
-void DXClient::initialize(WinImpl& winImpl)
+void DXClient::initialize(Window& window)
 {
 	// tearing supported
 	g_TearingSupported = DXFactory::CheckTearingSupport();
@@ -33,60 +34,50 @@ void DXClient::initialize(WinImpl& winImpl)
 	MS_ComPtr(IDXGIAdapter4) dxgiAdapter4 = DXFactory::GetAdapter(g_UseWarp);
 	g_Device = DXFactory::CreateDevice(dxgiAdapter4);
 
-	// command queue and swap chain
-	g_CommandQueue = DXFactory::CreateCommandQueue(g_Device, D3D12_COMMAND_LIST_TYPE_DIRECT);
-	g_SwapChain = DXFactory::CreateSwapChain(winImpl.g_hWnd, g_CommandQueue,
-		winImpl.g_ClientWidth, winImpl.g_ClientHeight, DXClient::g_NumFrames);
+	g_CommandRequester = std::make_unique<DXCommandRequester>(g_Device, D3D12_COMMAND_LIST_TYPE_DIRECT);
 
-	// RTV heap
+	g_SwapChain = DXFactory::CreateSwapChain(window.g_hWnd, g_CommandRequester->getCommandQueue(),
+		window.g_ClientWidth, window.g_ClientHeight, DXClient::g_NumFrames);
+
 	g_RTVDescriptorHeap = DXFactory::CreateDescriptorHeap(g_Device, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, DXClient::g_NumFrames);
 	g_RTVDescriptorSize = g_Device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 
 	// backBuffer+RTV in heap and commandAllocator
 	DXFactory::UpdateRenderTargetViews(g_Device, g_SwapChain, g_RTVDescriptorHeap, g_BackBuffers, DXClient::g_NumFrames);
-	for (int i = 0; i < DXClient::g_NumFrames; ++i)
-	{
-		g_CommandAllocators[i] = DXFactory::CreateCommandAllocator(g_Device, D3D12_COMMAND_LIST_TYPE_DIRECT);
-	}
 
-	// command List
 	g_CurrentBackBufferIndex = g_SwapChain->GetCurrentBackBufferIndex();
-	g_CommandList = DXFactory::CreateCommandList(g_Device,
-		g_CommandAllocators[g_CurrentBackBufferIndex], D3D12_COMMAND_LIST_TYPE_DIRECT);
-
-	// fence and fenceEvent
-	g_Fence = DXFactory::CreateFence(g_Device);
-	g_FenceEvent = DXFactory::CreateEventHandle();
 
 	g_IsInitialized = true;
 }
 
-uint64_t DXClient::signal(uint64_t& fenceValue)
+void DXClient::resize(Window& window,  uint32_t width, uint32_t height)
 {
-	uint64_t fenceValueForSignal = ++fenceValue;
-	ThrowIfFailed(g_CommandQueue->Signal(g_Fence.Get(), fenceValueForSignal));
-
-	return fenceValueForSignal;
-}
-
-void DXClient::waitForFenceValue( uint64_t fenceValue, HANDLE fenceEvent, std::chrono::milliseconds duration)
-{
-	if (g_Fence->GetCompletedValue() < fenceValue)
+	if (window.g_ClientWidth != width || window.g_ClientHeight != height)
 	{
-		ThrowIfFailed(g_Fence->SetEventOnCompletion(fenceValue, fenceEvent));
-		::WaitForSingleObject(fenceEvent, static_cast<DWORD>(duration.count()));
+		// Don't allow 0 size swap chain back buffers.
+		window.resize(width, height);
+
+		// Flush the GPU queue to make sure the swap chain's back buffers
+		// are not being referenced by an in-flight command list.
+		g_CommandRequester->flush();
+
+		for (int i = 0; i < g_NumFrames; ++i)
+		{
+			// Any references to the back buffers must be released
+			// before the swap chain can be resized.
+			g_BackBuffers[i].Reset();
+			g_FenceValues[i] = g_FenceValues[g_CurrentBackBufferIndex];
+		}
+
+		DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+		ThrowIfFailed(g_SwapChain->GetDesc(&swapChainDesc));
+		ThrowIfFailed(g_SwapChain->ResizeBuffers(g_NumFrames, window.g_ClientWidth, window.g_ClientHeight,
+			swapChainDesc.BufferDesc.Format, swapChainDesc.Flags));
+
+		g_CurrentBackBufferIndex = g_SwapChain->GetCurrentBackBufferIndex();
+
+		DXFactory::UpdateRenderTargetViews(g_Device, g_SwapChain, g_RTVDescriptorHeap, g_BackBuffers, g_NumFrames);
 	}
-}
-
-void DXClient::flush(uint64_t& fenceValue, HANDLE fenceEvent)
-{
-	uint64_t fenceValueForSignal = signal(fenceValue);
-	waitForFenceValue(fenceValueForSignal, fenceEvent);
-}
-
-void DXClient::flush_out()
-{
-	this->flush(g_FenceValue, g_FenceEvent);
 }
 
 void DXClient::update()
@@ -116,73 +107,37 @@ void DXClient::update()
 
 void DXClient::render()
 {
-	auto commandAllocator = g_CommandAllocators[g_CurrentBackBufferIndex];
 	auto backBuffer = g_BackBuffers[g_CurrentBackBufferIndex];
-
-	commandAllocator->Reset();
-	g_CommandList->Reset(commandAllocator.Get(), nullptr);
+	MS_ComPtr(ID3D12GraphicsCommandList) commandList = g_CommandRequester->getCommandList();
 	// Clear the render target.
 	{
 		CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(
 			backBuffer.Get(),
-			D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+			D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET
+		);
+		commandList->ResourceBarrier(1, &barrier);
 
-		g_CommandList->ResourceBarrier(1, &barrier);
 		FLOAT clearColor[] = { 0.4f, 0.6f, 0.9f, 1.0f };
 		CD3DX12_CPU_DESCRIPTOR_HANDLE rtv(g_RTVDescriptorHeap->GetCPUDescriptorHandleForHeapStart(),
 			g_CurrentBackBufferIndex, g_RTVDescriptorSize);
 
-		g_CommandList->ClearRenderTargetView(rtv, clearColor, 0, nullptr);
+		commandList->ClearRenderTargetView(rtv, clearColor, 0, nullptr);
 	}
 	// Present
 	{
 		CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(
 			backBuffer.Get(),
 			D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
-		g_CommandList->ResourceBarrier(1, &barrier);
-		ThrowIfFailed(g_CommandList->Close());
+		commandList->ResourceBarrier(1, &barrier);
+		g_FenceValues[g_CurrentBackBufferIndex]=g_CommandRequester->executeCommandList(commandList);
 
-		ID3D12CommandList* const commandLists[] = {
-			g_CommandList.Get()
-		};
-		g_CommandQueue->ExecuteCommandLists(_countof(commandLists), commandLists);
 		UINT syncInterval = g_VSync ? 1 : 0;
 		UINT presentFlags = g_TearingSupported && !g_VSync ? DXGI_PRESENT_ALLOW_TEARING : 0;
 		ThrowIfFailed(g_SwapChain->Present(syncInterval, presentFlags));
 
-		g_FrameFenceValues[g_CurrentBackBufferIndex] = signal(g_FenceValue);
 		g_CurrentBackBufferIndex = g_SwapChain->GetCurrentBackBufferIndex();
 
-		waitForFenceValue(g_FrameFenceValues[g_CurrentBackBufferIndex], g_FenceEvent);
+		g_CommandRequester->waitForFenceValue(g_FenceValues[g_CurrentBackBufferIndex]);
 	}
 }
 
-void DXClient::resize(WinImpl& winImpl,  uint32_t width, uint32_t height)
-{
-	if (winImpl.g_ClientWidth != width || winImpl.g_ClientHeight != height)
-	{
-		// Don't allow 0 size swap chain back buffers.
-		winImpl.resize(width, height);
-
-		// Flush the GPU queue to make sure the swap chain's back buffers
-		// are not being referenced by an in-flight command list.
-		flush(g_FenceValue, g_FenceEvent);
-
-		for (int i = 0; i < g_NumFrames; ++i)
-		{
-			// Any references to the back buffers must be released
-			// before the swap chain can be resized.
-			g_BackBuffers[i].Reset();
-			g_FrameFenceValues[i] = g_FrameFenceValues[g_CurrentBackBufferIndex];
-		}
-
-		DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
-		ThrowIfFailed(g_SwapChain->GetDesc(&swapChainDesc));
-		ThrowIfFailed(g_SwapChain->ResizeBuffers(g_NumFrames, winImpl.g_ClientWidth, winImpl.g_ClientHeight,
-			swapChainDesc.BufferDesc.Format, swapChainDesc.Flags));
-
-		g_CurrentBackBufferIndex = g_SwapChain->GetCurrentBackBufferIndex();
-
-		DXFactory::UpdateRenderTargetViews(g_Device, g_SwapChain, g_RTVDescriptorHeap, g_BackBuffers, g_NumFrames);
-	}
-}
--- DirectX12-Demo/DXClient.h
+++ DirectX12-Demo/DXClient.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdint.h>
 #include <chrono>
+#include <memory>
 #include <wrl.h> // Windows runtime library
 #ifndef MS_ComPtr
 #define MS_ComPtr(TYPE) Microsoft::WRL::ComPtr<TYPE>
@@ -9,32 +10,19 @@
 // DX12 spec
 #include <d3d12.h>
 #include <dxgi1_6.h> // DXGI1.6 adds HDR feature
-#include <d3dx12.h>
 
-#if defined(max)
-#undef max
-#endif
-#if defined(min)
-#undef min
-#endif
-
-class WinImpl;
+class Window;
+class DXCommandRequester;
 
 class DXClient {
 public:
 	DXClient(bool useWarp=false, bool vSync=false, bool tearing=false);
 	~DXClient();
-	void initialize(WinImpl& winImpl);
+	void initialize(Window& window);
+	void resize(Window& window, uint32_t width, uint32_t height);
 
-	uint64_t signal(uint64_t& fenceValue);
-	void waitForFenceValue(uint64_t fenceValue, HANDLE fenceEvent,
-		std::chrono::milliseconds duration = std::chrono::milliseconds::max());
-	void flush(uint64_t& fenceValue, HANDLE fenceEvent);
-
-	void flush_out();
 	void update();
 	void render();
-	void resize(WinImpl& winImpl, uint32_t width, uint32_t height);
 
 public:
 	const static uint8_t g_NumFrames = 3; // back buffers
@@ -45,22 +33,16 @@ public:
 	bool g_TearingSupported ;
 
 	// DirectX 12 Objects
-	MS_ComPtr(ID3D12Device2) g_Device1;
 	MS_ComPtr(ID3D12Device2) g_Device;
-	MS_ComPtr(ID3D12CommandQueue) g_CommandQueue;
 	MS_ComPtr(IDXGISwapChain4) g_SwapChain; // used to present
 	MS_ComPtr(ID3D12Resource) g_BackBuffers[g_NumFrames]; // pointer to back buffer resources
-	MS_ComPtr(ID3D12GraphicsCommandList) g_CommandList; // one per thread
-	MS_ComPtr(ID3D12CommandAllocator) g_CommandAllocators[g_NumFrames]; // backing memory for commands, sync with GPU execution->one per backbuffer
 	MS_ComPtr(ID3D12DescriptorHeap) g_RTVDescriptorHeap; // RTV(view=desc) location,type/dim of the RT(tex), 
 	UINT g_RTVDescriptorSize;
 	UINT g_CurrentBackBufferIndex;
 
-	// Sync Objects
-	MS_ComPtr(ID3D12Fence) g_Fence; // fence per command queue
-	uint64_t g_FenceValue ;
-	uint64_t g_FrameFenceValues[g_NumFrames] ;
-	HANDLE g_FenceEvent; // handle to osEvent
+	// CommandQueue
+	std::unique_ptr<DXCommandRequester> g_CommandRequester;
+	uint64_t g_FenceValues[g_NumFrames];
 
 	// Frametime
 	bool g_IsCounterInitialized;
