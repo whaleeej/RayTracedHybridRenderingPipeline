8610738e47c95f6ffbb4bff7c46b965bd2f9726f
ceej <ceej_7@hotmail.com>
2019-12-20 13:29:27 +0800

    feat: refactored to tutorial002

--- DirectX12-Demo/main.cpp
+++ DirectX12-Demo/main.cpp
@@ -1,141 +1,44 @@
-#define WIN32_LEAN_AND_MEAN // for Windows.h minimum included
+#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-#include <shellapi.h> // For CommandLineToArgvW after Windows.h
+#include <Shlwapi.h>
 
+#include "Application.h"
+#include "Game02.h"
 
-#include "Window.h"
-#include "DXFactory.h"
-#include "DXClient.h"
-#include "Helpers.h" // including after others
+#include <dxgidebug.h>
 
-Window window;
-DXClient dxClient;
+#define D3D12_CREATE_DEVICE_DEBUG
 
-void ParseCommandLineArguments()
+void ReportLiveObjects()
 {
-	int argc;
-	wchar_t** argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
+	IDXGIDebug1* dxgiDebug;
+	DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug));
 
-	for (size_t i = 0; i < argc; ++i)
-	{
-		if (::wcscmp(argv[i], L"-w") == 0 || ::wcscmp(argv[i], L"--width") == 0)
-		{
-			window.g_ClientWidth = ::wcstol(argv[++i], nullptr, 10);
-		}
-		if (::wcscmp(argv[i], L"-h") == 0 || ::wcscmp(argv[i], L"--height") == 0)
-		{
-			window.g_ClientHeight = ::wcstol(argv[++i], nullptr, 10);
-		}
-		if (::wcscmp(argv[i], L"-warp") == 0 || ::wcscmp(argv[i], L"--warp") == 0)
-		{
-			dxClient.g_UseWarp = true;
-		}
-	}
-
-	// Free memory allocated by CommandLineToArgvW
-	::LocalFree(argv);
-}
-
-void EnableDebugLayer()
-{
-#if defined(_DEBUG)
-	// Always enable the debug layer before doing anything DX12 related
-	// so all possible errors generated while creating DX12 objects
-	// are caught by the debug layer.
-	MS_ComPtr(ID3D12Debug) debugInterface;
-	ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface)));
-	debugInterface->EnableDebugLayer();
-#endif
-}
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	if (dxClient.g_IsInitialized)
-	{
-		switch (message)
-		{
-		case WM_PAINT:
-		{
-			dxClient.update();
-			dxClient.render();
-		}
-		break;
-		case WM_SYSKEYDOWN:
-		case WM_KEYDOWN:
-		{
-			bool alt = (::GetAsyncKeyState(VK_MENU) & 0x8000) != 0;
-
-			switch (wParam)
-			{
-			case 'V':
-				dxClient.g_VSync = !dxClient.g_VSync;
-				break;
-			case VK_ESCAPE:
-				::PostQuitMessage(0);
-				break;
-			case VK_RETURN:
-				if (alt)
-				{
-			case VK_F11:
-				window.setFullscreen(!window.g_Fullscreen);
-				}
-				break;
-			}
-		}
-		break;
-		case WM_SYSCHAR:
-			break;
-		case WM_SIZE:
-		{
-			RECT clientRect = {};
-			::GetClientRect(window.g_hWnd, &clientRect);
-
-			int width = clientRect.right - clientRect.left;
-			int height = clientRect.bottom - clientRect.top;
-
-			dxClient.resize(window , width, height);
-		}
-		break;
-		case WM_DESTROY:
-			::PostQuitMessage(0);
-			break;
-		default:
-			return ::DefWindowProcW(hwnd, message, wParam, lParam);
-		}
-	}
-	else
-	{
-		return ::DefWindowProcW(hwnd, message, wParam, lParam);
-	}
-
-	return 0;
+	dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_IGNORE_INTERNAL);
+	dxgiDebug->Release();
 }
 
 int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
 {
-	// Windows 10 Creators update adds Per Monitor V2 DPI awareness context.
-	// Using this awareness context allows the client area of the window 
-	// to achieve 100% scaling while still allowing non-client window content to 
-	// be rendered in a DPI sensitive fashion.
-	SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
-	ParseCommandLineArguments();
-	EnableDebugLayer();
+	int retCode = 0;
 
-	window.registerWindowClass(hInstance, &WndProc);
-	window.createWindow(hInstance);
-	//render loop with msg Callback
-	dxClient.initialize(window);
-	window.showWindow();
-
-	MSG msg = {};
-	while (msg.message != WM_QUIT)
+	// Set the working directory to the path of the executable.
+	WCHAR path[MAX_PATH];
+	HMODULE hModule = GetModuleHandleW(NULL);
+	if (GetModuleFileNameW(hModule, path, MAX_PATH) > 0)
 	{
-		if (::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-		{
-			::TranslateMessage(&msg);
-			::DispatchMessage(&msg);
-		}
+		PathRemoveFileSpecW(path);
+		SetCurrentDirectoryW(path);
 	}
 
-	return 0;
+	Application::Create(hInstance);
+	{
+		std::shared_ptr<Tutorial2> demo = std::make_shared<Tutorial2>(L"Learning DirectX 12 -  02", 1280, 720);
+		retCode = Application::Get().Run(demo);
+	}
+	Application::Destroy();
+
+	atexit(&ReportLiveObjects);
+
+	return retCode;
 }
