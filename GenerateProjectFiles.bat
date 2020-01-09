d37a5ee94e2c8272013d9a8cc76080d51c6806d2
ceej <ceej_7@hotmail.com>
2020-01-09 23:00:08 +0800

    feat: line up to tutorial's rendering lib

---
+++ GenerateProjectFiles.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+PUSHD %~dp0
+
+REM Update these lines if the currently installed version of Visual Studio is not 2017.
+SET CMAKE="cmake"
+SET CMAKE_GENERATOR="Visual Studio 16 2019"
+SET CMAKE_BINARY_DIR=build_vs2019
+
+MKDIR %CMAKE_BINARY_DIR% 2>NUL
+PUSHD %CMAKE_BINARY_DIR%
+
+%CMAKE% -G %CMAKE_GENERATOR% -Wno-dev "%~dp0"
+
+IF ERRORLEVEL 1 (
+    PAUSE
+) ELSE (
+    START LearningDirectX12.sln
+)
+
+POPD
+POPD
