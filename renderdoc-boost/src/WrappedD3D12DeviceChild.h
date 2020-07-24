ef6c4441dc54cb7fe20a48d22ed472c5dbcf4537
ceej <ceej_7@hotmail.com>
2020-07-25 03:21:41 +0800

    11

--- renderdoc-boost/src/WrappedD3D12DeviceChild.h
+++ renderdoc-boost/src/WrappedD3D12DeviceChild.h
@@ -6,7 +6,7 @@ RDCBOOST_NAMESPACE_BEGIN
 
 class WrappedD3D12Device;
 
-class WrappedD3D12ObjectBase {
+class WrappedD3D12ObjectBase { //interface for m_pReal and switchToDevice
 public:
 	WrappedD3D12ObjectBase(ID3D12Object* pReal): m_pReal(pReal){}
 	virtual ~WrappedD3D12ObjectBase();
@@ -21,8 +21,15 @@ protected:
 	ID3D12Object* m_pReal;
 };
 
-class WrappedD3D12Object: public WrappedD3D12ObjectBase, public  {
+template<typename NestedType>
+class WrappedD3D12Object: public WrappedD3D12ObjectBase, public NestedType{ // account for wrapped layer for object, also the base of all d3d12Class
+public:
+	WrappedD3D12Object(ID3D12Object* pReal) : WrappedD3D12ObjectBase(pReal), m_Ref(1)();
+	~WrappedD3D12ObjectBase();
 
+protected:
+	unsigned int m_Ref;
+	PrivateDataMap m_PrivateData;
 };
 
 
