99fb3dfca606d9fadd3b15e938af557cdd1d7c43
ceej7 <ceej_7@hotmail.com>
2020-08-03 17:07:17 +0800

    feat: update note

--- renderdoc-boost/src/RdcBoostPCH.h
+++ renderdoc-boost/src/RdcBoostPCH.h
@@ -17,7 +17,17 @@
 // func
 // framework
 
-// update 7/30 对comptr的理解
+//update 7/25 switchToDevice D3D12框架完成
+
+//update 7/27 完成 device和swapchain1的override
+
+//update 7/29 完成descriptor heap的缓存和拷贝， 完成最小化的所有wrapper
+
+//update 7/30 实现等帧逻辑，一帧最后present然后waitForFirstBackBuffer value后调用D3D12CallAtEndOfFrame，然后wrappedDevice中去做switch操作，首先要做的是等待所有fence的value跑完
+
+//update 7/30 初步实现d3d12 object的拷贝框架
+
+// update 7/31 对comptr的理解
 // comptr走指针init的话会把对象ref++
 // comptr走operator=对象会--原来的，把新的++
 // 但是用d3d的create，传入的无论是ptr还是comptr对象内存的ptr，都会生成一个ref=1的对象存在ptr里, 因为传入的是ppv，赋值不会给ref多＋1
@@ -31,9 +41,10 @@
 //在backRefs中记录指针，不持有生命周期，在Wrapper进行delete的时候自动从map中erase掉     ------>   RawPtr  
 //copyToDevice中创建的对象，改成ComPtr直接走operator=                                                       ------>   ComPtr
 
-// update 8/1 TODO
-// -1 CLEARRENDERTARGETVIEW_MISMATCHINGCLEARVALUE initvalue和clearvalue不一致 在开启renderdoc后会产生，很奇怪，要么是和swapchain那里有关，先关了。其实是因为debug layer被wrapped关掉功能了。本来就有这个output
-// 0. descriptor heap中的resource可能已经被释放了
+//update7/31 支持api传入参数为null的情况，主要支持了CreateView那部分
+
+// update  TODO
+// 0. descriptor heap中的resource可能已经被释放了 -》 catch(...)
 // 1. 支持CreateView中的参数情况
 // 2. 支持其他API的参数情况
 // 3. ID3D12Device1-6
