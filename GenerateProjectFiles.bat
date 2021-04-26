38442df1e166fd4d9d70ff29723ab7783f181255
ceej7 <ceej_7@hotmail.com>
2021-04-26 13:20:55 +0800

    feat: 修复资产问题

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -3,15 +3,15 @@
 PUSHD %~dp0
 
 SET CMAKE="cmake"
-SET CMAKE_GENERATOR="Visual Studio 16 2019"
+SET CMAKE_GENERATOR="Visual Studio 15 2017"
 SET CMAKE_BINARY_DIR=build_vs2019
 
 MKDIR %CMAKE_BINARY_DIR% 2>NUL
 PUSHD %CMAKE_BINARY_DIR%
 
-%CMAKE% -G %CMAKE_GENERATOR% -Wno-dev "%~dp0"
+%CMAKE% -A x64 -G %CMAKE_GENERATOR% -Wno-dev "%~dp0"
 
-cd D:\Repos\DX12-HybridPipeline
+cd G:\RepoCeej\DX12-HybridPipeline
 
 XCOPY .\DX12Lib\lib\dxcompiler\*.dll .\build_vs2019\bin\Release /e /I
 XCOPY .\DX12Lib\lib\dxcompiler\*.dll .\build_vs2019\bin\Debug /e /I
